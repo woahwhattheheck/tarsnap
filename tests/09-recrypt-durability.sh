c36b23c35ee1508d0b0f1d7d80a6a521112607f6
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-13 06:51:15 -0400

    tests: invoke recrypt durability harness via sh

--- tests/09-recrypt-durability.sh
+++ tests/09-recrypt-durability.sh
@@ -6,6 +6,6 @@ c_valgrind_min=9
 scenario_cmd() {
 	# Exercise recrypt durability ordering before old-machine deletion.
 	setup_check "check recrypt cache durability ordering"
-	"${scriptdir}/unit/recrypt-durability.sh" "${bindir}"
+	sh "${scriptdir}/unit/recrypt-durability.sh" "${bindir}"
 	echo $? > "${c_exitfile}"
 }
